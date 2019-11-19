eb44360a3a44880afd932678df0f66ca0ac60649
Maina Wycliffe <wmmaina7@gmail.com>
2019-11-19 19:34:31 +0300

    make the examples more verbose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,28 @@ import { SubscribeComponent } from './examples/subscribe/subscribe.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  content = 'Hello, who am i?';
+  content = 'A simple string content modal overlay';
   yesNoComponent = YesNoDialogComponent;
   subscribeComponent = SubscribeComponent;
+
+  subscribeData = null;
+  yesNoComponentResponse = null;
+  yesNoTemplateResponse = null;
 
   constructor(private overlayService: OverlayService) {}
 
   open(content: TemplateRef<any> | ComponentType<any> | string) {
     const ref = this.overlayService.open(content, null);
 
-    ref.afterClosed$.subscribe(data => {
-      console.log(data);
+    ref.afterClosed$.subscribe(res => {
+      if (typeof content === 'string') {
+      } else if (content === this.yesNoComponent) {
+        this.yesNoComponentResponse = res.data;
+      } else if (content === this.subscribeComponent) {
+        this.subscribeData = res.data;
+      } else {
+        this.yesNoTemplateResponse = res.data;
+      }
     });
   }
 }
