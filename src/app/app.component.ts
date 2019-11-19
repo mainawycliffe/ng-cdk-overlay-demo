f6b4d14ea29c5309909496b50c9ef4deb5e3e271
Maina Wycliffe <wmmaina7@gmail.com>
2019-11-19 19:12:12 +0300

    add ui for loading component based content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, TemplateRef } from '@angular/core';
 import { OverlayService } from './overlay.service';
+import { ComponentType } from '@angular/cdk/portal';
+import { YesNoDialogComponent } from './examples/yes-no-dialog/yes-no-dialog.component';
+import { SubscribeComponent } from './examples/subscribe/subscribe.component';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +11,12 @@ import { OverlayService } from './overlay.service';
 })
 export class AppComponent {
   content = 'Hello, who am i?';
+  yesNoComponent = YesNoDialogComponent;
+  subscribeComponent = SubscribeComponent;
 
   constructor(private overlayService: OverlayService) {}
 
-  open(content: TemplateRef<any> | string) {
+  open(content: TemplateRef<any> | ComponentType<any> | string) {
     const ref = this.overlayService.open(content, null);
 
     ref.afterClosed$.subscribe(data => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { OverlayComponent } from './overlay/overlay.component';
+import { YesNoDialogComponent } from './examples/yes-no-dialog/yes-no-dialog.component';
+import { SubscribeComponent } from './examples/subscribe/subscribe.component';
 
 @NgModule({
-  declarations: [AppComponent, OverlayComponent],
-  imports: [BrowserModule, OverlayModule],
+  declarations: [
+    AppComponent,
+    OverlayComponent,
+    YesNoDialogComponent,
+    SubscribeComponent
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, OverlayModule],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [OverlayComponent]
+  entryComponents: [OverlayComponent, YesNoDialogComponent, SubscribeComponent]
 })
 export class AppModule {}
